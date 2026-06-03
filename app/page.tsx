d316d097c5deee74692d43ad397950a0e7c0cc40
hm-sass <mdasom1106@gmail.com>
2026-06-03 13:20:03 +0900

    feat: add lemon squeezy checkout links

--- app/page.tsx
+++ app/page.tsx
@@ -436,7 +436,7 @@ export default function Home() {
         <li>Competitor radar</li>
         <li>Slack alerts</li>
       </ul>
-      <a href="/login" className="plan-cta">Start free trial</a>
+      <a href="https://respondai12.lemonsqueezy.com/buy/1739869" className="plan-cta" target="_blank">Start free trial</a>
     </div>
     <div className="plan">
       <div className="plan-name">Pro</div>
@@ -451,7 +451,7 @@ export default function Home() {
         <li>Custom AI tone</li>
         <li>API access</li>
       </ul>
-      <a href="/login" className="plan-cta">Go pro</a>
+      <a href="https://respondai12.lemonsqueezy.com/buy/1739870" className="plan-cta" target="_blank">Go pro</a>
     </div>
   </div>
 </section>
